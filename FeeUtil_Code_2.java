02fd1817111a6ee1eb3b86c6fc8df77b6a57658a
Marcel Gaiser <marcelg1235@gmail.com>
2025-06-01 09:28:04 +0200

    Update FeeUtil_Code_2.java

--- FeeUtil_Code_2.java
+++ FeeUtil_Code_2.java
@@ -3,16 +3,33 @@ package com.avento.shop.backend.util;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+/**
+ * Utility class for calculating shipping and article fees.
+ */
 public class FeeUtil implements IFeeUtil {
 
+	private static final BigDecimal ZERO = BigDecimal.ZERO;
+	private static final int SCALE = 5;
+	private static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_UP;
+
+	/**
+	 * Calculates the shipping and article fee per unit.
+	 *
+	 * @param priceShip    the shipping price
+	 * @param priceArticle the article price
+	 * @param feeTotal     the total fee
+	 * @param quantity     the quantity of items
+	 * @return a Fee object containing the calculated fees
+	 * @throws IllegalArgumentException if the price sum is negative
+	 */
 	@Override
 	public Fee calculateShipAndArticleFeePerUnit(BigDecimal priceShip, BigDecimal priceArticle, BigDecimal feeTotal, BigDecimal quantity) {
+		validateInputs(priceShip, priceArticle);
+
 		final Fee resultFees = new Fee();
 		final BigDecimal priceSum = priceShip.add(priceArticle);
 
-		validatePriceSum(priceSum);
-
-		if (priceSum.compareTo(BigDecimal.ZERO) == 0) {
+		if (priceSum.compareTo(ZERO) == 0) {
 			setZeroFees(resultFees);
 		} else {
 			calculateAndSetFees(priceShip, priceSum, feeTotal, quantity, resultFees);
@@ -21,24 +38,25 @@ public class FeeUtil implements IFeeUtil {
 		return resultFees;
 	}
 
-	private void validatePriceSum(BigDecimal priceSum) {
-		if (priceSum.compareTo(BigDecimal.ZERO) < 0) {
-			throw new RuntimeException("PriceSum [" + priceSum + "] is lower than 0!");
+	private void validateInputs(BigDecimal priceShip, BigDecimal priceArticle) {
+		final BigDecimal priceSum = priceShip.add(priceArticle);
+		if (priceSum.compareTo(ZERO) < 0) {
+			throw new IllegalArgumentException("PriceSum [" + priceSum + "] is lower than 0!");
 		}
 	}
 
 	private void setZeroFees(Fee resultFees) {
-		resultFees.setShipFee(BigDecimal.ZERO);
-		resultFees.setArticleFee(BigDecimal.ZERO);
+		resultFees.setShipFee(ZERO);
+		resultFees.setArticleFee(ZERO);
 	}
 
 	private void calculateAndSetFees(BigDecimal priceShip, BigDecimal priceSum, BigDecimal feeTotal, BigDecimal quantity, Fee resultFees) {
-		final BigDecimal factorToShip = priceShip.divide(priceSum, 5, RoundingMode.HALF_UP);
+		final BigDecimal factorToShip = priceShip.divide(priceSum, SCALE, ROUNDING_MODE);
 		final BigDecimal shipFee = feeTotal.multiply(factorToShip);
 		final BigDecimal articleFee = feeTotal.subtract(shipFee);
 
-		final BigDecimal shipFeePerItem = shipFee.divide(quantity, 2, RoundingMode.HALF_UP);
-		final BigDecimal articleFeePerItem = articleFee.divide(quantity, 2, RoundingMode.HALF_UP);
+		final BigDecimal shipFeePerItem = shipFee.divide(quantity, 2, ROUNDING_MODE);
+		final BigDecimal articleFeePerItem = articleFee.divide(quantity, 2, ROUNDING_MODE);
 
 		resultFees.setShipFee(shipFeePerItem);
 		resultFees.setArticleFee(articleFeePerItem);
